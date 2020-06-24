215a6a9ddbd9df2ef243b735c51c27c0d994e130
Igor Dovganyuk <thelscon@gmail.com>
2020-06-24 23:06:33 +0300

    final

---
+++ scrips.js
@@ -0,0 +1,9 @@
+'use strict';
+
+console.group ('Вывести в консоль произвольные значения 5 разными методами (имеется в виду методы объекта console): ');
+    console.log ('Вывод произвольного значения');
+    console.dir ('Объект');
+    console.info ('Информационное сообщение!');
+    console.warn ('Предупреждение!');
+    console.error ('Ошибка!');
+console.groupEnd ();
